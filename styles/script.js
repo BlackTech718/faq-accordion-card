ce61c8bc9da81ab60b06a90f3a17ff334471fbee
BlackTeck718 <nandoughcorp@live.com>
2020-10-20 19:54:27 -0400

    java code working 1

--- styles/script.js
+++ styles/script.js
@@ -4,5 +4,45 @@ document.getElementById("down_arrow").onclick = function() {
     function fun() {  
     document.getElementById("answer").style.display = "block";  
     // debugger
-    document.getElementById("down_arrow").style.rotate = "90deg";   
+    document.getElementById("down_arrow").style.transform = "rotate(180deg)";   
+    };
+
+
+document.getElementById("down_arrow1").onclick = function() {  
+    fun1();  
+    };  
+    function fun1() {  
+    document.getElementById("answer1").style.display = "block";  
+    // debugger
+    document.getElementById("down_arrow1").style.transform = "rotate(90deg)";   
+    };
+
+
+document.getElementById("down_arrow2").onclick = function() {  
+    fun2();  
+    };  
+    function fun2() {  
+    document.getElementById("answer2").style.display = "block";  
+    // debugger
+    document.getElementById("down_arrow2").style.transform = "rotate(90deg)";   
+    };
+
+
+document.getElementById("down_arrow3").onclick = function() {  
+    fun3();  
+    };  
+    function fun3() {  
+    document.getElementById("answer3").style.display = "block";  
+    // debugger
+    document.getElementById("down_arrow3").style.transform = "rotate(90deg)";   
+    };
+
+
+document.getElementById("down_arrow4").onclick = function() {  
+    fun4();  
+    };  
+    function fun4() {  
+    document.getElementById("answer4").style.display = "block";  
+    // debugger
+    document.getElementById("down_arrow4").style.transform = "rotate(90deg)";   
     };
