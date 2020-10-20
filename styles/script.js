52061847e8cca695cfd2672354d844d150556ac5
Fernando Beckworth <nandoughcorp@live.com>
2020-10-20 17:16:16 -0400

    work from home

---
+++ styles/script.js
@@ -0,0 +1,8 @@
+document.getElementById("down_arrow").onclick = function() {  
+    fun();  
+    };  
+    function fun() {  
+    document.getElementById("answer").style.display = "block";  
+    // debugger
+    document.getElementById("down_arrow").style.rotate = "90deg";   
+    };
