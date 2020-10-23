ad277d17b6d2bcc582fcae70525082b92b69b959
Fernando Beckworth <nandoughcorp@live.com>
2020-10-23 15:01:42 -0400

    more javascript changes

--- styles/app.js
+++ styles/app.js
@@ -2,62 +2,28 @@ var qblock = document.getElementsByClassName("question"); //variable/variable  n
 var i,x;
 var answers = document.getElementsByClassName("answer");
 
-
-
 for(i = 0; i< qblock.length; i++){
     qblock[i].addEventListener("click",function(){
-        for(i = 0 ; i< qblock.length; i++){
-            qblock[i].children[0].classList.remove("question_bold");
-            qblock[i].children[1].classList.remove("arrow");
-            qblock[i].children[2].classList.remove("answer_show");  
-        };
-            this.children[0].classList.add("question_bold"); //heavy font
-            this.children[1].classList.add("arrow"); //arrow down
-            this.children[2].classList.add("answer_show"); //show answer  
+        // for(i = 0 ; i< qblock.length; i++){
+        //     qblock[i].classList.remove("open");
+        //     qblock[i].children[0].classList.remove("boldf");
+        //     qblock[i].children[1].classList.remove("arrowUp");
+        // }; 
+        // debugger
+        if(this.classList.contains("open")){
+            //  for(i = 0 ; i< qblock.length; i++){
+                this.classList.remove("open");
+                // this.children[0].classList.remove("boldf");
+                // this.children[1].classList.remove("arrowUp");
+        // }; 
+
+        }else{
+            for(i = 0; i < qblock.length; i++){
+                qblock[i].classList.remove("open");
+            }
+            this.classList.add("open");
+            // this.children[0].classList.add("boldf");
+            // this.children[1].classList.add("arrowUp");
+        }
     });
 };
-
-
-
-// function opencheck(){
-//     var x;
-//     var qblock = document.getElementsByClassName("question"); //variable/variable  non literal.
-//     var answers = document.getElementsByClassName("answer")
-//     for(x=0; i< answers.length; i++){
-//         qblock[x].addEventListener("click", function(){
-//             if (answers[x].style.display === "block"){
-//                 answers[x].style.display = "none";
-//             }else{
-//                 answers[x].display = "block";
-//             }
-//         })
-//     }
-
-// }
-
-    
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// arrow.addEventListener("click", function(){
-//     var ocheck = qblock[2].children[2].style
-//     arrow.style.transform = "rotate(360deg)";
-//     qblock[0].children[0].classList.add("question_bold");
-//     qblock[0].children[2].style.display = "block";
-//     ocheck.display = "block";
-// })
-// qblock[0].children[0].classList.remove
